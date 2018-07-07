ca538dedcd807a70a06c03711ce315b8304c2f5c
justice roy rubi <justice_rubi@yahoo.com>
2018-07-07 17:54:45 +0800

    Add test

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
     });
-    console.log(req.body.text);
+    //console.log(req.body.text);
     todo.save().then((doc) => {
         res.send(doc);
     }, (e) => {
