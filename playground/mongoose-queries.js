cbb0a8e669b13382e9c86133239ba9d4f7e3f02d
justice roy rubi <justice_rubi@yahoo.com>
2018-07-07 16:13:35 +0800

    Add queries to playground

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,50 @@
+﻿//import { ObjectId } from './C:/Users/jqrubi/AppData/Local/Microsoft/TypeScript/2.6/node_modules/@types/bson';
+
+const { ObjectID } = require('mongodb');
+
+var { mongoose } = require('./../server/db/mongoose');
+var { Todo } = require('./../server/models/todo');
+var {User} = require('./../server/models/user');
+
+//var id = '5b3ec16ce3019b222039bb2011';
+var id = '5b3d991436c9062470b73f5a';
+
+//if (!ObjectID.isValid(id)) {
+//    console.log('ID not valid');
+//}
+
+//Todo.find({
+//    _id: id
+//}).then((todos) => {
+//    console.log('Todos', todos);
+//});
+
+//Todo.findOne({
+//    _id: id
+//}).then((todo) => {
+//    console.log('Todo', todo);
+//    });
+
+//Todo.findById(id).then((todo) => {
+//    if (!todo) {
+//        //return stops further execution
+//        return console.log('Id not found');
+//    }
+//    console.log('Todo By Id', todo);
+//}).catch((e) => console.log(e));
+
+//User.findById(id).then((user) => {
+//    if (!user) {
+//        return console.log('Id not found');
+//    }
+//    console.log('User by Id', user);
+//}).catch((e) => console.log(e));
+
+User.findById(id).then((user) => {
+    if (!user) {
+        return console.log('Id not found');
+    }
+    console.log(JSON.stringify(user, undefined, 2));
+}, (e) => {
+    console.log(e);
+});
--- server/server.js
+++ server/server.js
@@ -13,9 +13,20 @@ app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
     });
-
+    console.log(req.body.text);
     todo.save().then((doc) => {
         res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({
+            todos
+            //can add another property
+        });
     }, (e) => {
         res.status(400).send(e);
     });
@@ -24,6 +35,8 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
+
+module.exports = { app };
 
 //var newUser = new User({
 //    email: 'justiceroy.rubi@gmail.com'
